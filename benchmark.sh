8435bd1ae48f86085f418ba3b5d28248d58922f2
Ves5 <trinerxd@gmail.com>
2023-09-10 18:30:43 +0000

    calico, antrea, weave tests

--- benchmark.sh
+++ benchmark.sh
@@ -29,35 +29,57 @@ filename=deployments/deployment.yaml
     # run few iperf tests to get average
     POD=$(kubectl get pod -l app=iperf-client -o jsonpath="{.items[0].metadata.name}")
     IP=$(kubectl get pod -l app=iperf-server -o jsonpath="{.items[0].status.podIP}")
-    # echo "Using pod $POD for tests"
+    # SVC_IP_TCP=$(kubectl get svc iperf-server-tcp -o jsonpath="{.spec.clusterIP}")
+    # SVC_IP_UDP=$(kubectl get svc iperf-server-udp -o jsonpath="{.spec.clusterIP}")
     
     # iperf3 TCP tests
-    echo -e "\n[TCP] starting throughput tests\n"
+    echo -e "\n[TCP] starting pod-pod throughput tests\n"
     for i in {1..5}; do
         echo "Iteration $i"
-        kubectl exec -it ${POD} -- iperf3 -c $IP | grep sender | awk '{print $7, $9}' >> $savefile
+        kubectl exec -it ${POD} -- iperf3 -c $IP -O 2 | grep sender | awk '{print $7, $9}' >> $savefile
+        sleep 1
         # returns sender throughput and retries
     done
     # sleep for 5 seconds
     sleep 5
 
     # iperf3 UDP tests
-    echo -e "\n[UDP] starting throughput tests\n"
+    echo -e "\n[UDP] starting pod-pod throughput tests\n"
     for i in {1..5}; do
         echo "Iteration $i"
-        kubectl exec -it ${POD} -- iperf3 -c $IP -u -b 0 | grep receiver | awk '{print $7, $9, $12}' >> $savefile
+        kubectl exec -it ${POD} -- iperf3 -c $IP -u -b 0| grep receiver | awk '{print $7, $9, $12}' >> $savefile
+        sleep 1
         # returns receiver throughput, jitter and datagram loss
     done
     # sleep for 5 seconds
     sleep 5
 
     # Ping RTT tests
-    echo -e "\n[RTT] starting latency tests\n"
+    echo -e "\n[RTT] starting pod-pod latency tests\n"
     for i in {1..5}; do
         echo "Iteration $i"
         kubectl exec -it ${POD} -- hping3 $IP -c 50 -i u20000 -1 | tail -1 | awk '{print $4}' >> $savefile
+        sleep 1
         # returns just the values of min/avg/max from output of hping3 -> to be parsed later into csv
     done
+    # # sleep for 5 seconds
+    # sleep 5
+
+    # echo -e "\n[TCP] starting pod-svc throughput tests\n"
+    # for i in {1..5}; do
+    #     echo "Iteration $i"
+    #     kubectl exec -it ${POD} -- iperf3 -c $SVC_IP_TCP -O 2 | grep sender | awk '{print $7, $9}' >> $savefile
+    #     # returns sender throughput and retries
+    # done
+    # # sleep for 5 seconds
+    # sleep 5
+
+    # echo -e "\n[UDP] starting pod-svc throughput tests\n"
+    # for i in {1..5}; do
+    #     echo "Iteration $i"
+    #     kubectl exec -it ${POD} -- iperf3 -c $SVC_IP_UDP -u -b 0 | grep receiver | awk '{print $7, $9, $12}' >> $savefile
+    #     # returns receiver throughput, jitter and datagram loss
+    # done
 
     echo -e "\nCleaning up $filename deployment"
     # clean after deployment
