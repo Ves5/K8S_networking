29c3fd3d97c5e2d046d44504cf8d2ad95330f706
Ves5 <trinerxd@gmail.com>
2023-09-11 11:37:34 +0000

    updated parse script, benchmark for svc and extras

--- benchmark.sh
+++ benchmark.sh
@@ -28,58 +28,120 @@ filename=deployments/deployment.yaml
 
     # run few iperf tests to get average
     POD=$(kubectl get pod -l app=iperf-client -o jsonpath="{.items[0].metadata.name}")
-    IP=$(kubectl get pod -l app=iperf-server -o jsonpath="{.items[0].status.podIP}")
-    # SVC_IP_TCP=$(kubectl get svc iperf-server-tcp -o jsonpath="{.spec.clusterIP}")
-    # SVC_IP_UDP=$(kubectl get svc iperf-server-udp -o jsonpath="{.spec.clusterIP}")
+    # IP=$(kubectl get pod -l app=iperf-server -o jsonpath="{.items[0].status.podIP}")
     
     # iperf3 TCP tests
-    echo -e "\n[TCP] starting pod-pod throughput tests\n"
-    for i in {1..5}; do
-        echo "Iteration $i"
-        kubectl exec -it ${POD} -- iperf3 -c $IP -O 2 | grep sender | awk '{print $7, $9}' >> $savefile
-        sleep 1
-        # returns sender throughput and retries
-    done
-    # sleep for 5 seconds
-    sleep 5
-
-    # iperf3 UDP tests
-    echo -e "\n[UDP] starting pod-pod throughput tests\n"
-    for i in {1..5}; do
-        echo "Iteration $i"
-        kubectl exec -it ${POD} -- iperf3 -c $IP -u -b 0| grep receiver | awk '{print $7, $9, $12}' >> $savefile
-        sleep 1
-        # returns receiver throughput, jitter and datagram loss
-    done
-    # sleep for 5 seconds
-    sleep 5
-
-    # Ping RTT tests
-    echo -e "\n[RTT] starting pod-pod latency tests\n"
-    for i in {1..5}; do
-        echo "Iteration $i"
-        kubectl exec -it ${POD} -- hping3 $IP -c 50 -i u20000 -1 | tail -1 | awk '{print $4}' >> $savefile
-        sleep 1
-        # returns just the values of min/avg/max from output of hping3 -> to be parsed later into csv
-    done
-    # # sleep for 5 seconds
-    # sleep 5
-
-    # echo -e "\n[TCP] starting pod-svc throughput tests\n"
+    # echo -e "\n[TCP] starting pod-pod throughput tests\n"
     # for i in {1..5}; do
     #     echo "Iteration $i"
-    #     kubectl exec -it ${POD} -- iperf3 -c $SVC_IP_TCP -O 2 | grep sender | awk '{print $7, $9}' >> $savefile
+    #     kubectl exec -it ${POD} -- iperf3 -c $IP -O 2 | grep sender | awk '{print $7, $9}' >> $savefile
+    #     sleep 1
     #     # returns sender throughput and retries
     # done
     # # sleep for 5 seconds
     # sleep 5
 
-    # echo -e "\n[UDP] starting pod-svc throughput tests\n"
+    # # iperf3 UDP tests
+    # echo -e "\n[UDP] starting pod-pod throughput tests\n"
     # for i in {1..5}; do
     #     echo "Iteration $i"
-    #     kubectl exec -it ${POD} -- iperf3 -c $SVC_IP_UDP -u -b 0 | grep receiver | awk '{print $7, $9, $12}' >> $savefile
+    #     kubectl exec -it ${POD} -- iperf3 -c $IP -u -b 0| grep receiver | awk '{print $7, $9, $12}' >> $savefile
+    #     sleep 1
     #     # returns receiver throughput, jitter and datagram loss
     # done
+    # # sleep for 5 seconds
+    # sleep 5
+
+    # # Ping RTT tests
+    # echo -e "\n[RTT] starting pod-pod latency tests\n"
+    # for i in {1..5}; do
+    #     echo "Iteration $i"
+    #     kubectl exec -it ${POD} -- hping3 $IP -c 50 -i u20000 -1 | tail -1 | awk '{print $4}' >> $savefile
+    #     sleep 1
+    #     # returns just the values of min/avg/max from output of hping3 -> to be parsed later into csv
+    # done
+    # # sleep for 5 seconds
+    # sleep 5
+
+    # iperf3 pod-svc tests
+
+    kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: iperf-server-tcp
+spec:
+  selector:
+    app: iperf-server
+  ports:
+  - protocol: TCP
+    port: 5201
+    targetPort: 5201
+EOF
+
+    SVC_IP_TCP=$(kubectl get svc iperf-server-tcp -o jsonpath="{.spec.clusterIP}")
+    
+
+    echo -e "\n[TCP] starting pod-svc throughput tests\n"
+    for i in {1..5}; do
+        echo "Iteration $i"
+        kubectl exec -it ${POD} -- iperf3 -c $SVC_IP_TCP -O 2 | grep sender | awk '{print $7, $9}' >> $savefile
+        # returns sender throughput and retries
+    done
+
+    kubectl delete -f - <<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: iperf-server-tcp
+spec:
+  selector:
+    app: iperf-server
+  ports:
+  - protocol: TCP
+    port: 5201
+    targetPort: 5201
+EOF
+
+    # sleep for 5 seconds
+    sleep 5
+
+    kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: iperf-server-udp
+spec:
+  selector:
+    app: iperf-server
+  ports:
+  - protocol: UDP
+    port: 5201
+    targetPort: 5201
+EOF
+
+    SVC_IP_UDP=$(kubectl get svc iperf-server-udp -o jsonpath="{.spec.clusterIP}")
+
+    echo -e "\n[UDP] starting pod-svc throughput tests\n"
+    for i in {1..5}; do
+        echo "Iteration $i"
+        kubectl exec -it ${POD} -- iperf3 -c $SVC_IP_UDP -u -b 0 | grep receiver | awk '{print $7, $9, $12}' >> $savefile
+        # returns receiver throughput, jitter and datagram loss
+    done
+
+    kubectl delete -f - <<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: iperf-server-udp
+spec:
+  selector:
+    app: iperf-server
+  ports:
+  - protocol: UDP
+    port: 5201
+    targetPort: 5201
+EOF
 
     echo -e "\nCleaning up $filename deployment"
     # clean after deployment
--- parse.py
+++ parse.py
@@ -20,6 +20,13 @@ for file in files:
     avg_rtt = 0
     max_rtt = []
     
+    tcp_svc_bitrate = 0
+    tcp_svc_retries = 0
+    
+    udp_svc_bitrate = 0
+    udp_svc_jitter = 0
+    udp_svc_loss = 0
+    
     with open('results/' + file, 'r') as f:
         lines = f.readlines()
         
@@ -41,8 +48,8 @@ for file in files:
         udp_jitter = round(udp_jitter / 5, 3)
         udp_loss = round(udp_loss / 5, 2)
         
-        # parse last 5 lines of file       
-        for line in lines[10:]:
+        # parse third 5 lines of file       
+        for line in lines[10:15]:
             line = line.strip().split('/')
             min_rtt += [float(line[0])]
             avg_rtt += float(line[1])
@@ -50,12 +57,35 @@ for file in files:
         min_rtt = min(min_rtt)
         avg_rtt = round(avg_rtt / 5, 2)
         max_rtt = max(max_rtt)
+        
+        # parse fourth 5 lines of file
+        for line in lines[15:20]:
+            line = line.strip().split(' ')
+            tcp_svc_bitrate += float(line[0])
+            tcp_svc_retries += float(line[1])
+        tcp_svc_bitrate = round(tcp_svc_bitrate / 5, 2)
+        tcp_svc_retries = round(tcp_svc_retries / 5, 2)
+        
+        # parse last 5 lines of file
+        for line in lines[20:25]:
+            line = line.strip().split(' ')
+            udp_svc_bitrate += float(line[0])
+            udp_svc_jitter += float(line[1])
+            udp_svc_loss += float(line[2].strip('(').strip(')').strip('%'))
+        udp_svc_bitrate = round(udp_svc_bitrate / 5, 2)
+        udp_svc_jitter = round(udp_svc_jitter / 5, 3)
+        udp_svc_loss = round(udp_svc_loss / 5, 2)
     
-    values.append([file.split('.')[0], tcp_bitrate, tcp_retries, udp_bitrate, udp_jitter, udp_loss, min_rtt, avg_rtt, max_rtt])
+    values.append([file.split('.')[0], tcp_bitrate, tcp_retries, udp_bitrate, udp_jitter, udp_loss, min_rtt, avg_rtt, max_rtt, tcp_svc_bitrate, tcp_svc_retries, udp_svc_bitrate, udp_svc_jitter, udp_svc_loss])
     #print(f"{file}: {tcp_bitrate}, {tcp_retries}, {udp_bitrate}, {udp_jitter}, {udp_loss}, {min_rtt}, {avg_rtt}, {max_rtt}")
     
 # add to dataframe
-df = pd.DataFrame(values, columns=['CNI-Protocol', 'TCP Bitrate', 'TCP Retries', 'UDP Bitrate', 'UDP Jitter', 'UDP Loss', 'Min RTT', 'Avg RTT', 'Max RTT'])
+df = pd.DataFrame(values, columns=['CNI-Protocol', 
+                                   'TCP Bitrate', 'TCP Retries', 
+                                   'UDP Bitrate', 'UDP Jitter', 'UDP Loss', 
+                                   'Min RTT', 'Avg RTT', 'Max RTT', 
+                                   'TCP SVC Bitrate', 'TCP SVC Retries', 
+                                   'UDP SVC Bitrate', 'UDP SVC Jitter', 'UDP SVC Loss'])
 
 # save to csv
 df.to_csv('results.csv')   
